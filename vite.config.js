0c2457cc9e778e6cb201c64e6794f408081d57cb
prime_jd <ravishankartripathi2002@gmail.com>
2024-05-18 15:25:50 +0530

    ..

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/frontend/', 
+  plugins: [react()], 
   server : {
     
       proxy :{
