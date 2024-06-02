2994fd221b2dfe545a810f489f619dcc9265d5e1
prime_jd <ravishankartripathi2002@gmail.com>
2024-06-03 00:29:03 +0530

    vite configure

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()], 
+  build: {
+    outDir: 'dist', 
+  },
+  publicDir: 'public',
  // base : "/frontend/",
   server : {
     
