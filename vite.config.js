b4c002cb1c764d49d2bb315450535bb62814c8a6
prime_jd <ravishankartripathi2002@gmail.com>
2024-08-09 10:44:12 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,13 @@ export default defineConfig({
     host: true,
     strictPort: true,
     port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://server_image:8000', // or your API server URL
-        changeOrigin: true,
-        secure: false, // This disables SSL verification
-      },
-    },
-  },
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://server_image:8000', // or your API server URL
+  //       changeOrigin: true,
+  //       secure: false, // This disables SSL verification
+  //     },
+  //   },
+   },
   plugins: [react()],
 })
